6a85b15f787473533905753ce3ee8f2cff39a864
Hussain Jawadwala <hussain@jawadwala.com>
2018-11-20 14:28:52 +0530

    Update knet.php

--- application/config/knet.php
+++ application/config/knet.php
@@ -1,10 +1,12 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$config['knet_ClientId'] = 'YOUR_CLIENT_ID';
-$config['knet_ClientSecret'] = 'YOUR_CLIENT_SECRET';
-$config['knet_ENCRP_KEY'] = 'YOUR_ENCRYPTION_KEY';
+
+$config['knet_test_ClientId'] = 'YOUR_TEST_CLIENT_ID';
+$config['knet_test_ClientSecret'] ='YOUR_TEST_CLIENT_SECRET';
+$config['knet_test_ENCRP_KEY'] ="YOUR_TEST_CLIENT_ENC_KEY";
+$config['knet_test_url'] ='https://pgtest.cbk.com';
+$config['knet_live_ClientId'] = 'YOUR_CLIENT_ID';
+$config['knet_live_ClientSecret'] ='YOUR_CLIENT_SECRET';
+$config['knet_live_ENCRP_KEY'] ="YOUR_CLIENT_ENC_KEY";
+$config['knet_live_url'] ='https://pg.cbk.com';
+$config['knet_sandbox'] = true; //SET TRUE for live transaction
